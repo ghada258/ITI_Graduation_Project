818bdeac9402a84ea988b5b8ea9ac4f1cda9d75b
mai <maimahmud47@gmail.com>
2025-06-18 16:54:03 +0300

    not final

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import Navbar from './components/Navbar'
-
-
+import Contact from './pages/Contact'
 function App() {
  
 
   return (
     <>
       <Navbar/>
+
+      <Contact/>
+
     </>
   )
 }
